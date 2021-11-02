5fff305764208ef3c5da1f80d37ab3017f853c48
Evil Dave <evil.dave59@gmail.com>
2021-11-02 17:30:30 +0100

    version 05

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ tippy('[data-tippy-content]', {
   delay: [100, 200],
   hideOnClick: true,
 });
+
+var myModalEl = document.getElementById('addStep')
+myModalEl.addEventListener('hidden.bs.modal', function (event) {
+  // do something...
+})
